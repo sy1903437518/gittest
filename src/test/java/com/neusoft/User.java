23beabc25bc6e1bbfe653e0069ccb744fd3f78f7
‘sy1903437518 <zhangjinshan21f@11903437518@qq.com>
2019-03-05 13:11:37 +0800

    第一次修改的user

--- src/test/java/com/neusoft/User.java
+++ src/test/java/com/neusoft/User.java
@@ -2,6 +2,6 @@ package com.neusoft;
 
 public class User {
     public static void main(String[] args) {
-        System.out.println("gittｆｄｎｈｋｋ；刻录机了;ｈt");
+        System.out.println("gittｆｄｎｈ复合肥涣发大号笨女人他和我很有ｋｋ；ｈt");
     }
 }
