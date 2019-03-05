80cb73b8560fcc6e8931750d05128df33e5d2de7
‘sy1903437518 <zhangjinshan21f@11903437518@qq.com>
2019-03-05 12:49:17 +0800

    第二次修改的user

--- src/test/java/com/neusoft/User.java
+++ src/test/java/com/neusoft/User.java
@@ -2,6 +2,6 @@ package com.neusoft;
 
 public class User {
     public static void main(String[] args) {
-        System.out.println("我是测试git1111");
+        System.out.println("我是测试git11112233324444");
     }
 }
