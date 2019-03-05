3abc24984ddb265d3f932debc0435861002a3d39
‘sy1903437518 <zhangjinshan21f@11903437518@qq.com>
2019-03-05 13:06:00 +0800

    第一次修改的user

--- src/test/java/com/neusoft/User.java
+++ src/test/java/com/neusoft/User.java
@@ -2,6 +2,6 @@ package com.neusoft;
 
 public class User {
     public static void main(String[] args) {
-        System.out.println("1111我是测试gittttt");
+        System.out.println("我是测试gittｔｔｅｔｅt");
     }
 }
