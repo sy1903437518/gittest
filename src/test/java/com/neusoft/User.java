8add7b127cff6eec11422edc7c4688321fb42017
‘sy1903437518 <zhangjinshan21f@11903437518@qq.com>
2019-03-05 12:45:39 +0800

    第一次修改的user

--- src/test/java/com/neusoft/User.java
+++ src/test/java/com/neusoft/User.java
@@ -2,6 +2,6 @@ package com.neusoft;
 
 public class User {
     public static void main(String[] args) {
-        System.out.println("我是测试git");
+        System.out.println("我是测试git1111");
     }
 }
