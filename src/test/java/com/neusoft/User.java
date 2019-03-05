d5283cdd826a4fd8979a1f65e3f9ef47d88fa124
sy1903437518 <48229274+sy1903437518@users.noreply.github.com>
2019-03-05 13:28:24 +0800

    Update User.java

--- src/test/java/com/neusoft/User.java
+++ src/test/java/com/neusoft/User.java
@@ -2,6 +2,6 @@ package com.neusoft;
 
 public class User {
     public static void main(String[] args) {
-        System.out.println("gittｆｄｎｈ复合肥涣发大号笨女人他和我很有ｋｋ；ｈt");
+        System.out.println("gitt");
     }
 }
